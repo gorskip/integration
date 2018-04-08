476d713b7c5c8b04cbbf5d4cc08043fcabfd83ec
gorskip <prz.gorski@gmail.com>
2018-04-08 14:15:40 +0200

    added dot

--- app/src/main/java/com/pg/Main.java
+++ app/src/main/java/com/pg/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("### Application started with " + args.length + " args.");
-        System.out.println("### Application finished");
+        System.out.println("### Application finished.");
     }
 }
