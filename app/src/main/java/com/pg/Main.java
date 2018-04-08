0922a78eca37180e3d33c64c894ae5f94fd9f218
gorskip <prz.gorski@gmail.com>
2018-04-08 14:08:58 +0200

    add println

--- app/src/main/java/com/pg/Main.java
+++ app/src/main/java/com/pg/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("### Application started with " + args.length + " args.");
+        System.out.println("### Application finished");
     }
 }
