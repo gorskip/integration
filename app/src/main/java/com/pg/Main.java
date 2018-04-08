e39aa2e549a4e9477141003781dd26b6d08f8fd2
gorskip <prz.gorski@gmail.com>
2018-04-08 18:28:13 +0200

    added stupid code

--- app/src/main/java/com/pg/Main.java
+++ app/src/main/java/com/pg/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("### Application started with " + args.length + " args.");
-        System.out.println("### Application finished");
+
+        if (true && args.length == 0) {
+            System.out.println("WUT?");
+        }
     }
 }
