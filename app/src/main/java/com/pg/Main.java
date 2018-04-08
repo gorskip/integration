aa215dd92b5b8f089d309946a9bc5923e7f208ee
gorskip <prz.gorski@gmail.com>
2018-04-08 19:38:29 +0200

    removed stupid code

--- app/src/main/java/com/pg/Main.java
+++ app/src/main/java/com/pg/Main.java
@@ -4,9 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("### Application started with " + args.length + " args.");
-
-        if (true && args.length == 0) {
-            System.out.println("WUT?");
-        }
     }
 }
